49ba1fbba7ee4fdfbfe42be059916c757ba75b8f
Ehtesham <siddiquie.ehtesham@gmail.com>
2023-12-09 17:47:34 +0530

    Waku

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,26 +2,40 @@ import '@/styles/globals.css';
 import { Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
 import type { AppProps } from 'next/app';
-
-
+import { useEffect, useState } from 'react';
+import { LightNode } from '@waku/sdk';
+import { createNode } from '@/utils/waku';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Theme
-    // appearance='dark'
-    accentColor="gold"
-    grayColor="gray"
-    panelBackground="solid"
-    scaling="100%"
+  const [wakuNode, setWakuNode] = useState<LightNode | null>(null);
+  useEffect(() => {
+    if (wakuNode) return;
 
-  >
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;500&display=swap" rel="stylesheet" />
+    (async () => {
+      console.log('starting node');
+      const node = await createNode();
+      console.log('node started');
+      setWakuNode(node);
+    })();
+  }, [wakuNode]);
 
-    <div className="bg-[#faf8f3] text-[#646464]">
-      <div className="mx-auto max-w-screen-xl px-1 md:px-4 sm:px-6 relative">
-        <Component {...pageProps} />
+  return (
+    <Theme
+      // appearance='dark'
+      accentColor='gold'
+      grayColor='gray'
+      panelBackground='solid'
+      scaling='100%'
+    >
+      <div className='bg-[#faf8f3] text-[#646464]'>
+        <div className='mx-auto max-w-screen-xl px-1 md:px-4 sm:px-6 relative'>
+          {wakuNode ? (
+            <Component {...pageProps} node={wakuNode} />
+          ) : (
+            'Loading...'
+          )}
+        </div>
       </div>
-    </div>
-  </Theme>
+    </Theme>
+  );
 }
--- src/pages/claim/index.tsx
+++ src/pages/claim/index.tsx
@@ -1,64 +1,70 @@
-import Button from "@/components/UI/Button"
-import CardWrapper from "@/components/UI/CardWrapper"
-import PageLayout from "@/components/UI/PageLayout"
-import RewardCard from "@/components/shared/RewardCard"
-import { MagicWandIcon } from "@radix-ui/react-icons"
+'use client';
 
-const ClaimPage = () => {
-    return (
-        <main
-            className={` min-h-screen`}
-        >
-            <PageLayout
-                title='Claim Reward from 1 inch swap campaign'
-            />
+import Button from '@/components/UI/Button';
+import CardWrapper from '@/components/UI/CardWrapper';
+import PageLayout from '@/components/UI/PageLayout';
+import RewardCard from '@/components/shared/RewardCard';
+import { sendMessages } from '@/utils/waku';
+import { MagicWandIcon } from '@radix-ui/react-icons';
+import { createEncoder, createDecoder } from '@waku/sdk';
+import type { LightNode } from '@waku/sdk';
 
-            <CardWrapper title=''>
-                <div className="grid place-items-center">
-                    <div className="space-y-5 w-1/2 grid place-items-center">
-
-
-                        <h2 className='text-xl font-bold'>You got the reward 🎉</h2>
-
-                        <p>
-                            Thank you for participating in the 1 inch swap campaign.
-                        </p>
-                        <p>
-                            You have earned 0.1 ETH.
-                        </p>
-
-                        <Button>
-                            <MagicWandIcon className="mr-2" />
-                            Claim
-                        </Button>
-                    </div>
-                </div>
-            </CardWrapper>
-
-            <h5 className="text-2xl text-center mt-10 font-medium">
-                Other Rewards you can get
-            </h5>
-
-            <div className="grid grid-cols-3 gap-10 mt-10">
-                <RewardCard
-                    title="Be active on Lens"
-                    description="Follow more than 10 people on Lens and get 10 USDC"
-                    amount="10 USDC"
-                />
-                <RewardCard
-                    title="1 Swap on Uniswap"
-                    description="Swap one time on Uniswap and get 0.001 ETH reward"
-                    amount="0.001 ETH"
-                />
-                <RewardCard
-                    title="Borrow 10 USDC on Aave"
-                    description="Borrow 10 USDC on Aave and get 10 USDC reward"
-                    amount="10 USDC"
-                />
-            </div>
-
-        </main>
-    )
+interface Props {
+  node: LightNode;
 }
 
-export default ClaimPage
+const ClaimPage = ({ node }: Props) => {
+  return (
+    <main className={` min-h-screen`}>
+      <PageLayout title='Claim Reward from 1 inch swap campaign' />
+      <CardWrapper title=''>
+        <div className='grid place-items-center'>
+          <div className='space-y-5 w-1/2 grid place-items-center'>
+            <h2 className='text-xl font-bold'>You got the reward 🎉</h2>
+
+            <p>Thank you for participating in the 1 inch swap campaign.</p>
+            <p>You have earned 0.1 ETH.</p>
+
+            <Button
+              onClick={async () => {
+                console.log('calling sendMessage');
+                await sendMessages(node, {
+                  message: '0xE956F831Db3ccad88836162b491BCb733117faac|true',
+                  timestamp: new Date(),
+                });
+                console.log('sent message');
+              }}
+            >
+              <MagicWandIcon className='mr-2' />
+              Claim
+            </Button>
+          </div>
+        </div>
+      </CardWrapper>
+
+      <h5 className='text-2xl text-center mt-10 font-medium'>
+        Other Rewards you can get
+      </h5>
+
+      <div className='grid grid-cols-3 gap-10 mt-10'>
+        <RewardCard
+          title='Be active on Lens'
+          description='Follow more than 10 people on Lens and get 10 USDC'
+          amount='10 USDC'
+        />
+        <RewardCard
+          title='1 Swap on Uniswap'
+          description='Swap one time on Uniswap and get 0.001 ETH reward'
+          amount='0.001 ETH'
+        />
+        <RewardCard
+          title='Borrow 10 USDC on Aave'
+          description='Borrow 10 USDC on Aave and get 10 USDC reward'
+          amount='10 USDC'
+        />
+      </div>
+    </main>
+  );
+};
+
+export default ClaimPage;
